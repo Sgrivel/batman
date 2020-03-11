70f6a4b7c0c231498ddaabe2be6dfaed63c92258
Sgrivel <61620781+Sgrivel@users.noreply.github.com>
2020-03-11 17:29:11 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    let e1;
+
+    e1 = document.getElementsByTagName("div")[0];
+    // ou document.querySelector("div.nth-of-type(1)");
+    e1.style.color = "red";
+})
